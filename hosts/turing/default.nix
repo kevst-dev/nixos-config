57f2acff7d395a1418a0a4cb7ae490f2b4a9522d
kevst <kevinca100711@gmail.com>
2026-01-18 22:54:20 -0500

    feat: modularizar configuración de networking en Turing

--- hosts/turing/default.nix
+++ hosts/turing/default.nix
@@ -9,34 +9,12 @@
 
     # Herramientas a nivel de sistema
     ../../modules/common/podman.nix
+
+    # Configuraciones específicas de red del host
+    ./networking.nix
   ];
 
   # Configuración específica del servidor Turing
-  networking = {
-    hostName = "turing";
-
-    # Configuración de red estática
-    useDHCP = false;
-    interfaces.enp2s0.ipv4.addresses = [
-      {
-        address = "192.168.20.79";
-        prefixLength = 24;
-      }
-    ];
-    defaultGateway = "192.168.20.1";
-    nameservers = ["192.168.20.1"];
-
-    # Configuración de firewall
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [
-        22 # SSH
-        80 # HTTP (Traefik)
-        443 # HTTPS (Traefik)
-        8080 # Traefik Dashboard
-      ];
-    };
-  };
 
   # Configuración del usuario
   users.users.${username} = {
