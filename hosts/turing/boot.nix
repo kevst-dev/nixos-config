c2fa19061cfac2550b5fcd5d3aea5706eeda0e93
kevst <kevinca100711@gmail.com>
2026-01-19 10:53:35 -0500

    feat: modularizar configuración de boot en Turing

---
+++ hosts/turing/boot.nix
@@ -0,0 +1,11 @@
+{_}: {
+  # Configuración de boot (UEFI con systemd-boot)
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    # Permitir que Podman rootless use puertos < 1024 (necesario para Traefik)
+    kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
+  };
+}
